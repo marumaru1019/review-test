bfef6d33098ed96734e5c8d6873dc86e9b63fa60
REDMOND\ryuseioya <ryu.ryusei401@gmail.com>
2024-01-02 22:13:39 +0900

    change path

--- src/test.py
+++ src/test.py
@@ -25,7 +25,7 @@ def main():
         print(f"File: {file.filename}, Status: {file.status}")
 
     # コメントを追加
-    pr.create_comment("This is a comment.", last_commit, diff[0].filename, 0)
+    pr.create_comment("This is a comment.", last_commit, "sample.txt", 0)
 
 
     
