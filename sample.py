c822eba1cb70059c6046c6452f76982e9f70db71
REDMOND\ryuseioya <ryu.ryusei401@gmail.com>
2024-10-01 12:25:53 +0900

    add sample

---
+++ sample.py
@@ -0,0 +1,16 @@
+def calculate_ratio(a, b):
+    """
+    aとbの比率を計算する関数。
+    
+    Parameters:
+    a (float): 分子
+    b (float): 分母
+    
+    Returns:
+    float: a / b
+    """
+    return a / b
+
+# テストケース
+print(calculate_ratio(10, 2))
+print(calculate_ratio(5, 0))
